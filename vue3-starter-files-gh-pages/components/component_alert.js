2292dac21a1267ac01bfc4711a0ca7d85cb7e2a5
d93fu6513 <102431864+d93fu6513@users.noreply.github.com>
2022-05-13 19:30:50 +0800

    test

---
+++ vue3-starter-files-gh-pages/components/component_alert.js
@@ -0,0 +1,10 @@
+export default {
+    data() {
+        return {
+          text: '匯入外部元件'
+        };
+      },
+      template: `<div class="alert alert-primary" role="alert">
+        {{ text }}
+      </div>`
+}
